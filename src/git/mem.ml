c83c671329d9270e0ce2e76a34355b183ac52aaa
Hannes Mehnert <hannes@mehnert.org>
2023-06-21 08:43:10 +0200

    add some logs for timing

--- src/git/mem.ml
+++ src/git/mem.ml
@@ -74,7 +74,9 @@ let batch_write :
     if not (Hashtbl.mem store.values uid) then
       Hashtbl.add store.inflated uid (kind, inflated)
   in
-  iter index ~f
+  Log.info (fun m -> m "toplevel Mem.batch_write, now iterating");
+  iter index ~f;
+  Log.info (fun m -> m "toplevel Mem.batch_write, iterating done")
 
 let failuref fmt = Fmt.kstr (fun err -> Failure err) fmt
 
@@ -333,8 +335,10 @@ module Make (Digestif : Digestif.S) = struct
       let len = min (String.length pck_contents - pos) len in
       Bigstringaf.of_string ~off:pos ~len pck_contents
     in
+    Log.info (fun m -> m "Mem.batch_write: call to batch_write");
     batch_write t ~uid_ln:Hash.length ~uid_rw:Hash.of_raw_string ~map ~iter
       pck_contents index;
+    Log.info (fun m -> m "Mem.batch_write: done");
     Lwt.return_ok ()
 
   module Ref = struct
@@ -481,20 +485,25 @@ module Sync (Git_store : Minimal.S) = struct
     let index = Carton.Dec.Idx.Device.create t_idx in
     let src = Cstruct_append.key t_pck in
     let dst = Cstruct_append.key t_pck in
+    Log.info (fun m -> m "Sync.fetch starting");
     let create_idx_stream () =
       Carton.Dec.Idx.Device.project t_idx index
       |> Cstruct.of_bigarray
       |> stream_of_cstruct
     in
+    Log.info (fun m -> m "Sync.fetch starting 2");
     let create_pack_stream () =
       let pack = Cstruct_append.project t_pck dst in
       stream_of_cstruct pack
     in
+    Log.info (fun m -> m "Sync.fetch calling fetch");
     fetch ~push_stdout ~push_stderr ?threads ~ctx edn store ?version
       ?capabilities ?deepen want ~src ~dst ~idx:index ~create_idx_stream
       ~create_pack_stream t_pck t_idx
     >>= fun res ->
+    Log.info (fun m -> m "Sync.fetch called fetch");
     let _dst = Sys.opaque_identity dst in
     let _src = Sys.opaque_identity src in
+    Log.info (fun m -> m "Sync.fetch done");
     Lwt.return res
 end
